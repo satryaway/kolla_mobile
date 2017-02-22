f1ee81b2b87895846a534470219bbdb7e33c8a13
Satria Agung M <satryaway@gmail.com>
2017-02-22 09:34:53 +0700

    Fixed toolbar at main activity

--- app/src/main/java/com/jixstreet/kolla/Main2Activity.java
+++ app/src/main/java/com/jixstreet/kolla/Main2Activity.java
@@ -26,6 +26,9 @@ public class Main2Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
